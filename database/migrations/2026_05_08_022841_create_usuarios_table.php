4d31f2d51212a3dbd3ca17f699a1d4a7bb903e48
GabrieLlussi <gabriellussi05@gmail.com>
2026-05-07 23:30:07 -0300

    migrate usuarios

---
+++ database/migrations/2026_05_08_022841_create_usuarios_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+    Schema::create('usuarios', function (Blueprint $table) {
+        $table->id();
+        $table->string('nome');
+        $table->string('login')->unique();
+        $table->string('senha');
+        $table->string('situacao')->default('ativo');
+        $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+    Schema::dropIfExists('usuarios');
+    }
+};
