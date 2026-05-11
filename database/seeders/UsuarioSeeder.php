2fac03e07d705604276b13f0c708f1179543b58b
GabrieLlussi <gabriellussi05@gmail.com>
2026-05-10 22:55:54 -0300

    feat: adicionar migration e seeder usuarios

---
+++ database/seeders/UsuarioSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UsuarioSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('usuarios')->insert([
+        'nome' => 'admin',
+        'login' => 'admin',
+        'senha' => '123456',
+        'created_at' => now(),
+        'updated_at' => now(),
+        ]);
+    }
+}
