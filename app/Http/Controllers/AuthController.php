17c896a1717b0fecd98470d0cab80e6053637063
GabrieLlussi <gabriellussi05@gmail.com>
2026-06-22 20:21:40 -0300

    feat: ajustes no auth controller

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Usuario;
+use Illuminate\Support\Facades\Hash;
+
 
 class AuthController extends Controller
 {
@@ -14,16 +16,14 @@ class AuthController extends Controller
 
     public function login(Request $request)
     {
-        $user = Usuario::where('login', $request->login)
-                        ->where('senha', $request->senha)
-                        ->first();
+    $user = Usuario::where('login', $request->login)->first();
 
-        if ($user) {
-            session(['usuario' => $user]);
-            return redirect('/lancamentos');
-        }
+    if ($user && Hash::check($request->senha, $user->senha)) {
+        session(['usuario' => $user]);
+        return redirect('/lancamentos');
+    }
 
-        return back()->with('erro', 'Login inválido');
+    return back()->with('erro', 'Login inválido');
     }
 
     public function logout()
