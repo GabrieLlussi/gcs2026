3a23920c19d7fe4163c5477745066edc3022f196
GabrieLlussi <gabriellussi05@gmail.com>
2026-06-24 21:32:09 -0300

    teste de alteração e barrar no QA

--- resources/views/lancamentos/index.blade.php
+++ resources/views/lancamentos/index.blade.php
@@ -83,7 +83,7 @@ body {
 
 <!-- HEADER -->
 <div class="d-flex justify-content-between align-items-center mb-4">
-    <h3 class="fw-bold text-primary">💰 Controle financeiro</h3>
+    <h3 class="fw-bold text-primary">Controle financeiro</h3>
     <a href="/logout" class="btn btn-danger btn-modern">Sair</a>
 </div>
 
